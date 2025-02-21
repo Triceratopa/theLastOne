2e6cd5174774f07f35ccb7e31dc64f017f4033f5
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-02-21 10:43:47 +0100

    Initial commit

---
+++ src/main/java/com/reservation/theLastOne/TheLastOneApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.theLastOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLastOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLastOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservation/theLastOne/TheLastOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.theLastOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLastOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
