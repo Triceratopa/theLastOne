c75a3d53a1df5f43fb072a668675a23cfa5e5120
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-02-21 12:50:29 +0100

    .

--- src/main/java/eventList/EventRequest.java
+++ src/main/java/eventList/EventRequest.java
@@ -1,4 +1,17 @@
 package eventList;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventRequest {
+    private String name;
+    private String description;
+    private String location;
+    private String date;
+    private int capacity;
+    private Long organizerId;
 }
--- src/main/java/eventList/EventResponse.java
+++ src/main/java/eventList/EventResponse.java
@@ -1,4 +1,18 @@
 package eventList;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String location;
+    private String date;
+    private int capacity;
+
 }
